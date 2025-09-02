e122869e8e42087f96fa2a09808de5783a0d5eab
Rafael Almeida <dealmeida.silva@hotmail.com>
2025-09-02 15:19:50 -0300

    google

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { HelmetProvider } from "@dr.pogodin/react-helmet";
 
+import { Analytics } from "@vercel/analytics/react";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -12,5 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </HelmetProvider>
     </Provider>
+      <Analytics />
+
   </React.StrictMode>
 );
